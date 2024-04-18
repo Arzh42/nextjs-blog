7e5405e510c5b5d1bce9e6bd9768f2d927a1fd10
Arzh <hugo@chataigner.com>
2024-04-18 09:59:09 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { StyledText, TextField } from "@stoati/nextjs-tools";
 import Link from "next/link";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="flex flex-col justify-between items-center h-screen py-8">
       <header className="flex flex-col items-center gap-2 ">
         <h1 className="font-bold text-5xl text-white">
-          <TextField componentId="title" />
+          <TextField componentId="2b3cf892-d9d7-4145-a5d1-f42901f01fd8" />
         </h1>
         <h2 className="font-bold text-xl text-white">Artiste - Peintre</h2>
       </header>
@@ -20,7 +20,7 @@ export default async function Home() {
           src={`images/img1.jpg`}
           alt="Image 1"
         />
-        <StyledText componentId="image_desc" />
+        <StyledText componentId="addf6147-a0d5-43bc-bbb9-cdbac2d90115" />
       </div>
       <footer className="text-white font-bold">
         <Link href="https://www.instagram.com/baptiste__borel/">Instagram</Link>
