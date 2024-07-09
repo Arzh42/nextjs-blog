ad635d8a616f2628fc7382ad539c15495d84aec2
Arzh <hugo@chataigner.com>
2024-07-09 18:08:35 +0200

    Fix

--- app/Gallery.tsx
+++ app/Gallery.tsx
@@ -32,25 +32,26 @@ const Gallery = ({ parentId }: { parentId: string }) => {
                   className="bg-transparent object-contain relative"
                   key={props.parentId}
                   id={props.parentId}
-                  index={index}
                 >
-                  <Asset
-                    {...props}
-                    className="w-full h-full object-contain"
-                    templateElementId="c7215aa9-41a5-49b4-b6d8-372eb7cd8bd8"
-                  />
-                  <div className="absolute text-white bottom-2 left-0 right-0 flex justify-center">
-                    <div className="bg-black/75 rounded-xl p-4 flex flex-col items-center max-w-96 text-center">
-                      <TextField
-                        {...props}
-                        templateElementId="2f1b35e1-e0a5-4e43-bab7-2cd3dd8f821d"
-                      />
-                      <StyledText
-                        {...props}
-                        templateElementId="d789fd1b-4b47-4784-8725-84c1bc90849e"
-                      />
+                  <>
+                    <Asset
+                      {...props}
+                      className="w-full h-full object-contain"
+                      templateElementId="c7215aa9-41a5-49b4-b6d8-372eb7cd8bd8"
+                    />
+                    <div className="absolute text-white bottom-2 left-0 right-0 flex justify-center">
+                      <div className="bg-black/75 rounded-xl p-4 flex flex-col items-center max-w-96 text-center">
+                        <TextField
+                          {...props}
+                          templateElementId="2f1b35e1-e0a5-4e43-bab7-2cd3dd8f821d"
+                        />
+                        <StyledText
+                          {...props}
+                          templateElementId="d789fd1b-4b47-4784-8725-84c1bc90849e"
+                        />
+                      </div>
                     </div>
-                  </div>
+                  </>
                 </SliderMainItem>
               );
             }}
